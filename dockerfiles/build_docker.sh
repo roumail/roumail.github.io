b221e2e537d1f1748881f0b6da763da187132799
Rohail Taimour <rohail.taimour@gmail.com>
2023-05-20 17:11:54 +0200

    Update build version

--- dockerfiles/build_docker.sh
+++ dockerfiles/build_docker.sh
@@ -3,7 +3,12 @@
 set -a
 DOCKERHUB_IMAGE="$1"
 
-docker build --no-cache --platform linux/amd64 -t ${DOCKERHUB_IMAGE} .
+docker build --no-cache \
+    --platform linux/amd64 \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --build-arg USERNAME=$(whoami) \
+    -t ${DOCKERHUB_IMAGE} .
 
 # Run an interactive session with the built image
 # docker run --rm -it ${DOCKERHUB_USERNAME}/${DOCKERHUB_REPO}:${DOCKERHUB_TAG} bash
