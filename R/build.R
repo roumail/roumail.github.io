1e3a16ce91d468589cc0483630d5aac587af6a01
Rohail Taimour <rohail.taimour@gmail.com>
2023-05-23 01:02:59 +0200

    Add build script again

--- R/build.R
+++ R/build.R
@@ -1,13 +1,19 @@
 #/usr/bin/env Rscript
 
-# Install packages
-install.packages(c('blogdown', 'remotes'))
+# Check if packages are installed. Install them if they are not already installed.
+packages <- c('blogdown', 'remotes')
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
 
 # Get Hugo version from vercel.json
 hugo_version <- system2('jq', args = c('-r', '.build.env.HUGO_VERSION', 'vercel.json'), stdout = TRUE)
 
-# Install Hugo
-blogdown::install_hugo(version = hugo_version)
+# Check if Hugo is installed
+if (!blogdown::find_hugo()) {
+  # Install Hugo
+  blogdown::install_hugo(version = hugo_version)
+}
 
 # Build the site
-blogdown::build_site(TRUE)
+blogdown::build_site(local = FALSE)
