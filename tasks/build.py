cef529be9993a96bc9bf2567f42b4153a53836ce
Rohail Taimour <rohail.taimour@gmail.com>
2023-08-09 17:21:54 +0200

    Add logging

--- tasks/build.py
+++ tasks/build.py
@@ -1,10 +1,12 @@
 import os
 from invoke import task
-
+import logging
 from .utils.config import read_config
 from .rosetta import check_rosetta
 from .utils.constants import construct_image_name
+from .utils.logging import get_logger, LoggerFile
 
+log = get_logger(__name__)
 
 @task(pre=[check_rosetta])
 def build(c):
@@ -17,12 +19,15 @@ def build(c):
     gid = os.getgid()
     username = os.getlogin()
     image_name = construct_image_name(tag)
+    log_file = LoggerFile(log, logging.INFO)
 
     c.run(
         f"docker build --no-cache --platform linux/amd64 "
         f"--build-arg UID={uid} "
         f"--build-arg GID={gid} "
         f"--build-arg USERNAME={username} "
-        f"-t {image_name} ."
+        f"-t {image_name} .",
+        out_stream = log_file,
+        err_stream=log_file
     )
-    c.run(f"docker push {image_name}")
+    c.run(f"docker push {image_name}", out_stream=log_file, err_stream=log_file)
---
+++ tasks/utils/logging.py
@@ -0,0 +1,61 @@
+import importlib_resources
+import logging
+import logging.config
+import os
+
+import yaml
+
+
+def get_logger(module_name):
+    parts = module_name.split(".")
+
+    # Use only the last part (the actual module name) as the logger name
+    logger_name = parts[-1]
+
+    logger = logging.getLogger(logger_name)
+
+    # Check if logging has been initialized
+    if not logging.getLogger().hasHandlers():
+        initialize_logging()
+
+    return logger
+
+
+def initialize_logging(
+    log_dir="var/log",
+):
+    # Get the base directory from an environment variable
+    if not os.path.exists(log_dir):
+        print(f"specified log_directory doesn't exist: {log_dir}. Creating directory..")
+        os.makedirs(log_dir, exist_ok=True)
+
+    # Use the default path to the config file
+    path2log_config = importlib_resources.files("tasks").joinpath(
+        "etc/logging.yml"
+    )
+
+    # Load the logging configuration file
+    with open(path2log_config) as f:
+        config = yaml.safe_load(f)
+
+    # Replace the {LOG_DIR} placeholder with the actual base directory
+    for _, handler in config["handlers"].items():
+        if "filename" in handler:
+            log_directory = handler["filename"].format(LOG_DIR=log_dir)
+            handler["filename"] = log_directory
+
+    logging.config.dictConfig(config)
+
+    return log_dir
+
+class LoggerFile:
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+
+    def write(self, message):
+        for line in message.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
