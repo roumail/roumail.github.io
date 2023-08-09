50bc97460c5275fb374500ef298138100cdfa80f
Rohail Taimour <rohail.taimour@gmail.com>
2023-08-09 16:49:48 +0200

    rename as package

---
+++ tasks/__init__.py
@@ -0,0 +1,12 @@
+from invoke import Collection
+from .build import build as build_task
+from .install import install as install_task
+from .launch_rstudio import launch_rstudio as launch_rstudio_task
+from .rosetta import check_rosetta as check_rosetta_task
+
+# Create a collection and add the tasks to it
+namespace = Collection()
+namespace.add_task(build_task, name="build")
+namespace.add_task(install_task, name="install")
+namespace.add_task(launch_rstudio_task, name="launch-rstudio")
+namespace.add_task(check_rosetta_task, name="check-rosetta")
--- website/tasks.py
+++
@@ -1,8 +0,0 @@
-from .build import build
-from .install import install
-from .launch_rstudio import launch_rstudio
-from .rosetta import check_rosetta
-
-# from .cleanup import clean_previous_install
-
-# Add more imports as needed
