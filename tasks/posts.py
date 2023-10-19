41673d159433f7c4a8feb71bb5b1b50c84e7ed69
Rohail Taimour <rohail.taimour@gmail.com>
2023-10-19 17:13:34 +0200

    add quarto support

--- tasks/posts.py
+++ tasks/posts.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from invoke import task
 
+
 def update_series(post_path, series):
     index_file_path = os.path.join(post_path, "index.md")
     with open(index_file_path, "r") as f:
@@ -16,6 +17,7 @@ def update_series(post_path, series):
             else:
                 f.write(line)
 
+
 def harmonize_name(name):
     """
     Harmonize the given name by removing colons and replacing spaces with hyphens.
@@ -24,11 +26,15 @@ def harmonize_name(name):
     name = name.replace(" ", "-")  # Replace spaces with hyphens
     return name.lower()
 
-@task(help={
-    'title': 'The title of the blog post you want to create.',
-    'series': 'The series to which the blog post belongs (optional).'
-})
-def new_post(c, title, series=None):
+
+@task(
+    help={
+        "title": "The title of the blog post you want to create.",
+        "series": "The series to which the blog post belongs (optional).",
+        "quarto": "Initialize a quarto_document",
+    }
+)
+def new_post(c, title, series=None, quarto=False):
     """
     Create a new post with a given title under a specified category.
     Optionally, specify a series name.
@@ -44,34 +50,36 @@ def new_post(c, title, series=None):
 
     # Determine the path where the new blog post should be created
     if series:
-        path = f"content/post/{series_harmonized}/{timestamped_dir}"
+        post_dir = f"content/post/{series_harmonized}/{timestamped_dir}"
     else:
-        path = f"content/post/{timestamped_dir}"
+        post_dir = f"content/post/{timestamped_dir}"
 
     # Create the directory
-    os.makedirs(path, exist_ok=True)
+    os.makedirs(post_dir, exist_ok=True)
 
     # Create the new Hugo blog post
-    c.run(f"hugo new --kind blog-post {path}/index.md")
-    print(f"New post created at {path}/index.md")
+    path2post = f"{post_dir}/index.md"
+    c.run(f"hugo new --kind blog-post {path2post}")
+    print(f"New post created at {path2post}")
 
     # Check if a series is specified
     if series:
-        series_path = f"content/post/{series_harmonized}"
+        path2series_dir = f"content/post/{series_harmonized}"
 
         # Update the series front matter in index.md
-        update_series(path, series_harmonized)
+        update_series(post_dir, series_harmonized)
 
-        print(f"Updated series front matter for '{series}' at {path}/index.md")
+        print(f"Updated series front matter for '{series}' at {path2post}")
 
         # Path to the _index.md file
-        index_file_path = os.path.join(series_path, "_index.md")
+        index_file_path = os.path.join(path2series_dir, "_index.md")
 
         # Check if _index.md already exists
         if not os.path.exists(index_file_path):
             # Create _index.md with the specified content
             with open(index_file_path, "w") as f:
-                f.write(f"""---
+                f.write(
+                    f"""---
 title: {series}
 cms_exclude: true
 draft: true
@@ -81,5 +89,6 @@ draft: true
 #   2 = Compact
 #   3 = Card
 view: 2
-""")
+"""
+                )
             print(f"_index.md for series '{series}' created at {index_file_path}")
