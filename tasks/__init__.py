1d3dcb1e045ebe6eae34bef867832b5a7b5476a9
Rohail Taimour <rohail.taimour@gmail.com>
2024-04-03 23:07:21 +0200

    remove task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,8 @@
 from invoke import Collection
 
-from .posts import new_post, list_draft_posts
-from .release import tag_version
+from .posts import list_draft_posts, new_post
 
 # Create a collection and add the tasks to it
 namespace = Collection()
 namespace.add_task(new_post, name="new-post")
 namespace.add_task(list_draft_posts, name="list-draft-posts")
-namespace.add_task(tag_version, name="push-package-release")
