043c08eb003a37ab18d9ab3146b1ab860652c8db
Rohail Taimour <rohail.taimour@gmail.com>
2024-08-02 12:48:38 +0500

    update dependencies

--- tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from invoke import Collection
-
-from .posts import list_draft_posts, new_post
-
-# Create a collection and add the tasks to it
-namespace = Collection()
-namespace.add_task(new_post, name="new-post")
-namespace.add_task(list_draft_posts, name="list-draft-posts")
---
+++ website/entrypoints.py
@@ -0,0 +1,35 @@
+from website.posts import concatenate_drafts, create_new_post
+from website.task_helpers import check_project_root
+
+
+def new_post(title: str, series=None, quarto=False):
+    """
+    Create a new post with a given title under a specified category.
+    Optionally, specify a series name.
+    Params:
+        "title": "The title of the blog post you want to create."
+        "series": "The series to which the blog post belongs (optional)."
+        "quarto": "Initialize a quarto_document, defaults to False"
+    """
+    check_project_root()
+    create_new_post(title, series, quarto)
+
+
+def list_draft_posts(
+    file_out="current_draft_posts.md",
+    search_directory="content/post",
+    search_string="draft: true",
+):
+    """
+    Concatenate all draft posts into a single markdown file.
+    Params
+        "file_out": "The output file name for concatenated drafts. Defaults to 'concatenated_drafts.md'."
+        "search_directory": "The directory to search for draft posts. Defaults to 'content/post'."
+        "search_string": "The string to search for in the markdown files to identify drafts. Defaults to 'draft: true'."
+    """
+    check_project_root()
+    concatenate_drafts(
+        file_out=file_out,
+        search_directory=search_directory,
+        search_string=search_string,
+    )
