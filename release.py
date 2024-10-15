2cd7c52601f41dea976a02366f25a790dce33bfd
Rohail Taimour <rohail.taimour@gmail.com>
2024-10-15 08:30:52 +0200

    release again

--- release.py
+++ release.py
@@ -22,42 +22,6 @@ def bump_version(bump_type):
     return new_version
 
 
-def delete_tag(tag):
-    # Delete the local and remote tag if it exists
-    print(f"Deleting existing tag {tag} locally and remotely...")
-    subprocess.run(["git", "tag", "-d", tag], check=True)
-    subprocess.run(["git", "push", "origin", f":refs/tags/{tag}"], check=True)
-
-
-def tag_exists_on_remote(tag):
-    result = subprocess.run(
-        ["git", "ls-remote", "--tags", "origin", tag], capture_output=True, text=True
-    )
-    return bool(result.stdout.strip())
-
-
-def update_git(new_version: str, delete_existing_tag: bool = False):
-    # Fetch tags to ensure the local repo is up to date
-    subprocess.run(["git", "fetch", "--tags"], check=True)
-    tag_name = f"v{new_version}"
-
-    if tag_exists_on_remote(tag_name):
-        if delete_existing_tag:
-            delete_tag(tag_name)
-        else:
-            print(f"Tag {tag_name} already exists on the remote. Aborting!")
-            sys.exit(1)
-
-    subprocess.run(
-        ["git", "commit", "-am", f"Bump version to {new_version}"], check=True
-    )
-
-    subprocess.run(
-        ["git", "tag", tag_name, "-m", f"Release v{new_version}"], check=True
-    )
-    subprocess.run(["git", "push", "origin", tag_name], check=True)
-
-
 def main():
     bump_type = "patch"
 
@@ -69,9 +33,7 @@ def main():
 
     # Bump the version
     new_version = bump_version(bump_type)
-
-    # Commit and tag the changes
-    update_git(new_version)
+    print(f"Bumped version to {new_version}")
 
 
 if __name__ == "__main__":
